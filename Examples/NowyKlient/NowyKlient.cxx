23294459ac3237229f18949380ccab8bce9ea35d
Student <student@ib.polsl.pl>
2017-03-13 13:25:07 +0100

    NowyKlient i NowySerwer - poprawki

--- Examples/NowyKlient/NowyKlient.cxx
+++ Examples/NowyKlient/NowyKlient.cxx
@@ -1,13 +1,13 @@
 /*=========================================================================
 
-  Program:   OpenIGTLink -- Example for Tracker Client Program
-  Language:  C++
+Program:   OpenIGTLink -- Example for Tracker Client Program
+Language:  C++
 
-  Copyright (c) Insight Software Consortium. All rights reserved.
+Copyright (c) Insight Software Consortium. All rights reserved.
 
-  This software is distributed WITHOUT ANY WARRANTY; without even
-  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-  PURPOSE.  See the above copyright notices for more information.
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
 
@@ -18,93 +18,156 @@
 #include "igtlOSUtil.h"
 #include "igtlPointMessage.h"
 #include "igtlClientSocket.h"
+#include "igtlServerSocket.h"
 
 
 int main(int argc, char* argv[])
 {
-  //------------------------------------------------------------
-  // Parse Arguments
+	//------------------------------------------------------------
+	// Parse Arguments
 
-  if (argc != 3) // check number of arguments
-    {
-    // If not correct, print usage
-    std::cerr << "Usage: " << argv[0] << " <hostname> <port> <fps>"    << std::endl;
-    std::cerr << "    <hostname> : IP or host name"                    << std::endl;
-    std::cerr << "    <port>     : Port # (18944 in Slicer default)"   << std::endl;
-    exit(0);
-    }
+	if (argc != 3) // check number of arguments
+	{
+		// If not correct, print usage
+		std::cerr << "Usage: " << argv[0] << " <hostname> <port> <fps>"    << std::endl;
+		std::cerr << "    <hostname> : IP or host name"                    << std::endl;
+		std::cerr << "    <port>     : Port # (18944 in Slicer default)"   << std::endl;
+		exit(0);
+	}
 
-  char*  hostname = argv[1];
-  int    port     = atoi(argv[2]);
+	char*  hostname = argv[1];
+	int    port     = atoi(argv[2]);
 
-  //------------------------------------------------------------
-  // Establish Connection
+	//------------------------------------------------------------
+	// Establish Connection
 
-  igtl::ClientSocket::Pointer socket;
-  socket = igtl::ClientSocket::New();
-  int r = socket->ConnectToServer(hostname, port);
+	igtl::ClientSocket::Pointer socket;
+	socket = igtl::ClientSocket::New();
+	int r = socket->ConnectToServer(hostname, port);
 
-  if (r != 0)
-    {
-    std::cerr << "Cannot connect to the server." << std::endl;
-    exit(0);
-    }
+	if (r != 0)
+	{
+		std::cerr << "Cannot connect to the server." << std::endl;
+		exit(0);
+	}
 
-  //------------------------------------------------------------
-  // Allocate Transform Message Class
+	//------------------------------------------------------------
+	// Allocate Transform Message Class
 
-  igtl::PointMessage::Pointer pointMsg;
-  pointMsg = igtl::PointMessage::New();
-  pointMsg->SetDeviceName("PointSender");
+	igtl::PointMessage::Pointer pointMsg;
+	pointMsg = igtl::PointMessage::New();
+	pointMsg->SetDeviceName("PointSender");
 
-  //---------------------------
-  // Create 1st point
-  igtl::PointElement::Pointer point0;
-  point0 = igtl::PointElement::New();
-  point0->SetName("POINT_0");
-  point0->SetGroupName("GROUP_0");
-  point0->SetRGBA(0xFF, 0x00, 0x00, 0xFF);
-  point0->SetPosition(10.0, 20.0, 30.0);
-  point0->SetRadius(15.0);
-  point0->SetOwner("IMAGE_0");
-  
-  //---------------------------
-  // Create 2nd point
-  igtl::PointElement::Pointer point1;
-  point1 = igtl::PointElement::New();
-  point1->SetName("POINT_1");
-  point1->SetGroupName("GROUP_0");
-  point1->SetRGBA(0x00, 0xFF, 0x00, 0xFF);
-  point1->SetPosition(40.0, 50.0, 60.0);
-  point1->SetRadius(45.0);
-  point1->SetOwner("IMAGE_0");
-  
-  //---------------------------
-  // Create 3rd point
-  igtl::PointElement::Pointer point2;
-  point2 = igtl::PointElement::New();
-  point2->SetName("POINT_2");
-  point2->SetGroupName("GROUP_0");
-  point2->SetRGBA(0x00, 0x00, 0xFF, 0xFF);
-  point2->SetPosition(70.0, 80.0, 90.0);
-  point2->SetRadius(75.0);
-  point2->SetOwner("IMAGE_0");
-  
-  //---------------------------
-  // Pack into the point message
-  pointMsg->AddPointElement(point0);
-  pointMsg->AddPointElement(point1);
-  pointMsg->AddPointElement(point2);
-  pointMsg->Pack();
-  
-  //------------------------------------------------------------
-  // Send
-  socket->Send(pointMsg->GetPackPointer(), pointMsg->GetPackSize());
-  
-  
-  //------------------------------------------------------------
-  // Close the socket
-  socket->CloseSocket();
+	//---------------------------
+	// Create 1st point
+	igtl::PointElement::Pointer point0;
+	point0 = igtl::PointElement::New();
+	point0->SetName("POINT_0");
+	point0->SetGroupName("GROUP_0");
+	point0->SetRGBA(0xFF, 0x00, 0x00, 0xFF);
+	point0->SetPosition(10.0, 20.0, 30.0);
+	point0->SetRadius(15.0);
+	point0->SetOwner("IMAGE_0");
+
+	//---------------------------
+	// Create 2nd point
+	igtl::PointElement::Pointer point1;
+	point1 = igtl::PointElement::New();
+	point1->SetName("POINT_1");
+	point1->SetGroupName("GROUP_0");
+	point1->SetRGBA(0x00, 0xFF, 0x00, 0xFF);
+	point1->SetPosition(40.0, 50.0, 60.0);
+	point1->SetRadius(45.0);
+	point1->SetOwner("IMAGE_0");
+
+	//---------------------------
+	// Create 3rd point
+	igtl::PointElement::Pointer point2;
+	point2 = igtl::PointElement::New();
+	point2->SetName("POINT_2");
+	point2->SetGroupName("GROUP_0");
+	point2->SetRGBA(0x00, 0x00, 0xFF, 0xFF);
+	point2->SetPosition(70.0, 80.0, 90.0);
+	point2->SetRadius(75.0);
+	point2->SetOwner("IMAGE_0");
+
+	//---------------------------
+	// Pack into the point message
+	pointMsg->AddPointElement(point0);
+	pointMsg->AddPointElement(point1);
+	pointMsg->AddPointElement(point2);
+	pointMsg->Pack();
+
+	//------------------------------------------------------------
+	// Send
+	socket->Send(pointMsg->GetPackPointer(), pointMsg->GetPackSize());
+
+	igtl::ServerSocket::Pointer serverSocket;
+	serverSocket = igtl::ServerSocket::New();
+	int ser = serverSocket->CreateServer(port);
+
+	igtl::Socket::Pointer socketS;
+
+
+
+	while(1){
+
+		socketS = serverSocket->WaitForConnection(1000);
+
+		if(socketS.IsNotNull()){
+
+			igtl::MessageHeader::Pointer headerMsg;
+			headerMsg = igtl::MessageHeader::New();
+
+			headerMsg->InitPack();
+
+			int r = socketS->Receive(headerMsg->GetPackPointer(), headerMsg->GetPackSize());
+			if (r == 0)
+			{
+				socketS->CloseSocket();
+			}
+			if (r != headerMsg->GetPackSize())
+			{
+				continue;
+			}
+
+			headerMsg->Unpack();
+
+			//igtl::PointMessage::Pointer pointMsg;
+			//socketS->Receive(pointMsg->GetPackBodyPointer(), pointMsg->GetPackBodySize());
+			//int unpack = headerMsg->Unpack();
+
+			pointMsg = igtl::PointMessage::New();
+			pointMsg->SetMessageHeader(headerMsg);
+			pointMsg->AllocatePack();
+
+			igtl::PointElement::Pointer pointElement;
+			pointMsg->GetPointElement(0, pointElement);
+
+			igtlUint8 rgba[4];
+			pointElement->GetRGBA(rgba);
+
+			igtlFloat32 pos[3];
+			pointElement->GetPosition(pos);
+
+			//std::cerr << "========== Element #" << i << " ==========" << std::endl;
+			std::cerr << " Name      : " << pointElement->GetName() << std::endl;
+			std::cerr << " GroupName : " << pointElement->GetGroupName() << std::endl;
+			std::cerr << " RGBA      : ( " << (int)rgba[0] << ", " << (int)rgba[1] << ", " << (int)rgba[2] << ", " << (int)rgba[3] << " )" << std::endl;
+			std::cerr << " Position  : ( " << std::fixed << pos[0] << ", " << pos[1] << ", " << pos[2] << " )" << std::endl;
+			std::cerr << " Radius    : " << std::fixed << pointElement->GetRadius() << std::endl;
+			std::cerr << " Owner     : " << pointElement->GetOwner() << std::endl;
+			//std::cerr << "AAA dasd as"<< std::endl;
+			std::cerr << "================================" << std::endl;
+
+		}
+
+	}
+
+
+	//------------------------------------------------------------
+	// Close the socket
+	socket->CloseSocket();
 
 }
 
